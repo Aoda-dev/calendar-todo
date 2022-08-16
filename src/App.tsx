af45c68cc7229f2f7c3a2977b3295c6814cfd64e
Tolegen Kulseitov <hinokamiaoda@gmail.com>
2022-08-16 16:25:41 +0600

    inital react typescript tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="App">
+			<h1 className="text-7xl text-center mt-14 font-hero">Hello React App with TypeScript</h1>
+		</div>
+	);
+}
+
+export default App;
